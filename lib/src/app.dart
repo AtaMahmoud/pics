1d7c45a08971232bc1d36d2a50c4dda40946dd3d
Ata Mahmoud <ata.ata250@hotmail.com>
2018-08-13 00:32:13 +0200

    add fetch image and image list to the app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   @override
@@ -6,7 +10,18 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter=0;
+  int id = 0;
+  List<ImageModel>images=[];
+  void fetchImage() async {
+    id++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$id');
+    var imageModel=ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,15 +30,17 @@ class _AppState extends State<App> {
           title: Text("Let's See Images"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: ()=>setState(
-                  ()=>counter++
-          ),
+          onPressed: fetchImage,
           tooltip: "Add Image",
-          child: Icon(Icons.add,size: 40.0,),
+          child: Icon(
+            Icons.add,
+            size: 40.0,
+          ),
         ),
-        body: Center(
-          child: Text("$counter images"),
-        ),
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: ImageList(images),
+        )
       ),
     );
   }
